a966aad52dd1013848414a0c5608daeca14da63c
cs187staff <dengyuntian@g.harvard.edu>
2020-09-21 19:20:34 +0000

    automatic update

--- tests/prob_hamilton_examples.py
+++ tests/prob_hamilton_examples.py
@@ -1,6 +1,6 @@
 test = {   'name': 'prob_hamilton_examples',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> prob_hamilton_example0 > 0 and prob_hamilton_example9 > 0\nTrue', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> (0 < prob_hamilton_example0 < 1) and (0 < prob_hamilton_example9 < 1)\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
